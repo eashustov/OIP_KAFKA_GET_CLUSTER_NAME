0d54b38fb59520c8160a77b1df1e78b4de9bd19a
eashustov <eashustov@gmail.com>
2024-02-10 18:41:03 +0300

    Добавил классы реализации интерфейсов репозиториев с разделением на профили prod и dev

--- src/main/java/ru/sberbank/cip_corax_get_cluster_name/repo/cipsidecrepo/CIPSIDECRepo.java
+++ src/main/java/ru/sberbank/cip_corax_get_cluster_name/repo/cipsidecrepo/CIPSIDECRepo.java
@@ -1,11 +1,8 @@
 package ru.sberbank.cip_corax_get_cluster_name.repo.cipsidecrepo;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.sberbank.cip_corax_get_cluster_name.domain.CIPSIDECData;
-
-import java.util.List;
 
 @Repository
 public interface CIPSIDECRepo extends CrudRepository<CIPSIDECData, String> {
--- src/main/java/ru/sberbank/cip_corax_get_cluster_name/repo/cipskeeperrepo/CIPSkeeperRepo.java
+++ src/main/java/ru/sberbank/cip_corax_get_cluster_name/repo/cipskeeperrepo/CIPSkeeperRepo.java
@@ -1,11 +1,8 @@
 package ru.sberbank.cip_corax_get_cluster_name.repo.cipskeeperrepo;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.sberbank.cip_corax_get_cluster_name.domain.CIPSkeeperData;
-
-import java.util.List;
 
 @Repository
 public interface CIPSkeeperRepo extends CrudRepository<CIPSkeeperData, String> {
